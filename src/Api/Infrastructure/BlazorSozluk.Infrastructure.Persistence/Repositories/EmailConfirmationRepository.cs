b72401036c5827082e9705026a84e63be9a4df30
culinalionur <onurakinci53@gmail.com>
2023-03-01 15:00:30 +0300

    Repository added

---
+++ src/Api/Infrastructure/BlazorSozluk.Infrastructure.Persistence/Repositories/EmailConfirmationRepository.cs
@@ -0,0 +1,18 @@
+﻿using BlazorSozluk.Api.Domain.Models;
+using BlazorSozluk.Application.Interfaces.Repositories;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlazorSozluk.Infrastructure.Persistence.Repositories
+{
+    public class EmailConfirmationRepository : GenericRepository<EmailConfirmation>, IEmailConfirmationRepository
+    {
+        public EmailConfirmationRepository(DbContext dbContext) : base(dbContext)
+        {
+        }
+    }
+}
---
+++ src/Api/Infrastructure/BlazorSozluk.Infrastructure.Persistence/Repositories/EntryCommentRepository.cs
@@ -0,0 +1,18 @@
+﻿using BlazorSozluk.Api.Domain.Models;
+using BlazorSozluk.Application.Interfaces.Repositories;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlazorSozluk.Infrastructure.Persistence.Repositories
+{
+    public class EntryCommentRepository : GenericRepository<EntryComment>, IEntryCommentRepository
+    {
+        public EntryCommentRepository(DbContext dbContext) : base(dbContext)
+        {
+        }
+    }
+}
